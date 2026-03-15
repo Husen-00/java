e17393a0b9e95ec055de83fff16e9ab92b771542
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-03-16 01:01:16 +0530

    Add Fibonacci sequence calculation in fibonaci.java

---
+++ fibonaci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class fibonaci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int a =0;
+        int b = 1;
+        int c = 0;
+        for(int i=2; i<=n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println("The fibonaci of " + n + "th number is " + c);
+        sc.close();
+    }
+}
