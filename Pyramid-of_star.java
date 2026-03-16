245e6b543103d39b22d3d006ad6047e0ec25b66a
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-03-17 01:50:31 +0530

    Rename Pyramid class to Pyramid_of_star

--- Pyramid-of_star.java
+++ Pyramid-of_star.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pyramid {
+public class Pyramid_of_star {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.print("Enter the number of rows: ");
