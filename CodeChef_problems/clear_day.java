a9e3cf99577674f4fd06d02b4d9be5ae054514e4
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-11 01:37:37 +0530

    Add clear_day.java for calculating remaining days
    Implement a program to calculate remaining days.

---
+++ CodeChef_problems/clear_day.java
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int x = input.nextInt();
+	    int y = input.nextInt();
+	    System.out.println(7-(x+y));
+	}
+}
