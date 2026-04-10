98502da358969ea8db97587925091a8c2c557733
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-10 17:03:39 +0530

    Add IPL ticket rush problem solution in Java

---
+++ CodeChef_problems/Ipl_ticket_rush.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int n,m;
+	        n = input.nextInt();
+	        m = input.nextInt();
+	        if(n>m) {
+	        System.out.println(n-m);
+	        }
+	        else {
+	            System.out.println(0);
+	        }
+	    }
+	}
+}
