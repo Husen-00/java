105de222fca3fef1ac7583891274fabb44254f04
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-12 00:42:40 +0530

    Add Double_Rent.java for calculating double rent

---
+++ CodeChef_problems/Double_Rent.java
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int x = input.nextInt();
+	    System.out.println(2*x);
+	    input.close();
+	}
+}
