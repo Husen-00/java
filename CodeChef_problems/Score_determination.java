6aad9ebaf299870e83967e552705fec83ca587d4
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-15 15:04:52 +0530

    Add Score_determination.java for score calculation

---
+++ CodeChef_problems/Score_determination.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int x = input.nextInt();
+	        int n = input.nextInt();
+	        System.out.println(x/10 * n);
+	    }
+	}
+}
