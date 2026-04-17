e8f6f9d836dbe75d94eeed7d77d86162fd45faa4
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-17 21:59:57 +0530

    Implement basic input and output for OFFBY1 problem
    Fix the output format to correctly display the sum and '1'.

---
+++ off_By_one.java
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int a = input.nextInt();
+	    int b = input.nextInt();
+	    System.out.println(a+b+"1");
+	}
+}
