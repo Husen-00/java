0f490c78242495f0852b1012b7a4a549a552248f
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-18 16:21:18 +0530

    Add IPL_ticket_Rush.java solution for ticket problem

---
+++ CodeChef_problems/IPL_ticket_Rush.java
@@ -0,0 +1,23 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/IPLTRSH
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int n,m;
+	        n = input.nextInt();
+	        m = input.nextInt();
+	        if(n>m) {
+	        System.out.println(n-m);
+	        }
+	        else {
+	            System.out.println(0);
+	        }
+	    }
+	}
+}
