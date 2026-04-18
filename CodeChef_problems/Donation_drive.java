d5380c384da946f563400c68d89ebaddf9046b19
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-18 16:18:19 +0530

    Implement Donation Drive solution in Java

---
+++ CodeChef_problems/Donation_drive.java
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DONDRIVE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int n = input.nextInt();
+	        int x = input.nextInt();
+	        System.out.println(n-x);
+	    }
+	}
+}
