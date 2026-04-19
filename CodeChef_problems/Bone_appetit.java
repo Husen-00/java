cf63f52316047df637e2760ab2ab10cad9ceffe6
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-20 01:12:31 +0530

    Add Bone_appetit.java for CodeChef problem solution

---
+++ CodeChef_problems/Bone_appetit.java
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int n = input.nextInt();
+	    int m = input.nextInt();
+	    int x = input.nextInt();
+	    int y = input.nextInt();
+	    System.out.println((n*x) + (m*y));
+	}
+}
