9a9961d23c94c88a08efd2be0454cf72752d3be0
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-26 03:18:09 +0530

    Add TotalPrice_money.java for prize pool calculation

---
+++ CodeChef_problems/TotalPrice_money.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x, y;
+	        x = input.nextInt();
+	        y = input.nextInt();
+	        System.out.println((10*x) + (90*y));
+	    }
+	}
+}
