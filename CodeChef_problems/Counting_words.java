0338ca20d2895b47c0c67ad0103e01f5680f3947
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-04-27 01:03:16 +0530

    Add Counting_words.java for word counting problem

---
+++ CodeChef_problems/Counting_words.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int n, m;
+	        n = input.nextInt();
+	        m = input.nextInt();
+	        System.out.println(n*m);
+	    }
+	}
+}
