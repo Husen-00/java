16f8e7a43f7a3d98be2e328d978a7c86ccdfe5af
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-02 00:20:58 +0530

    Add Bull_or_Bear.java for profit/loss calculation

---
+++ CodeChef_problems/Bull_or_Bear.java
@@ -0,0 +1,26 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BULLBEAR
+import java.util.*
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int x, y;
+	        x = input.nextInt();
+	        y = input.nextInt();
+	        if(y>x) {
+	            System.out.println("PROFIT");
+	        }
+	        else if (x>y) {
+	            System.out.println("LOSS");
+	        }
+	        else {
+	            System.out.println("NEUTRAL");
+	        }
+	    }
+	}
+}
