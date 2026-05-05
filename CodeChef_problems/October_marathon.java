ed1ce997c9dceeea89ac9590ad3d31c0a40316aa
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-06 01:42:57 +0530

    Add October_marathon.java for CodeChef problem

---
+++ CodeChef_problems/October_marathon.java
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OCTATHON
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int x = input.nextInt();
+	    if (x<3) {
+	        System.out.println("GOLD");
+	    }
+	    else if(x>=3 && x<6) {
+	        System.out.println("SILVER");
+	    }
+	    else {
+	        System.out.println("BRONZE");
+	    }
+	}
+}
