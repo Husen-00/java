4dd02d6de17231b25b4757cadf3bf31b1729a2b5
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-18 01:26:49 +0530

    Add Sleep_deprivation.java solution

---
+++ CodeChef_problems/Sleep_deprivation.java
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SLEEP
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x = input.nextInt();
+	        if (x>=7) {
+	            System.out.println("NO");
+	        }
+	        else {
+	            System.out.println("YES");
+	        }
+	    }
+	}
+}
