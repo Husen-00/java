9cc7fc4780a28d28c5544f72520c4cc4ba381286
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-19 02:52:27 +0530

    Add Math_enrolment.java for CodeChef problem solution

---
+++ CodeChef_problems/Math_enrolment.java
@@ -0,0 +1,25 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/M1ENROL
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x, y;
+	        x =input.nextInt();
+	        y = input.nextInt();
+	        if (x>y) {
+	            System.out.println(0);
+	        }
+	        else {
+	        System.out.println(y-x);
+	        }
+	    }
+	}
+}
+
