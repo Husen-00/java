7186ddd6cd26989d9653325dcf06aaf2d9ad68d2
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-20 01:15:42 +0530

    Add Chef_and_chapters.java for CodeChef problem

---
+++ CodeChef_problems/Chef_and_chapters.java
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t= input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x, y,z;
+	        x = input.nextInt();
+	        y = input.nextInt();
+	        z = input.nextInt();
+	        System.out.println(x*y*z);
+	    }
+	}
+}
