15e6408f6c06352391f8d944ad21fde330630b00
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-21 00:45:55 +0530

    Implement water requirement calculation in Java

---
+++ CodeChef_problems/Water_requirment.java
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t= input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int n =input.nextInt();
+	        System.out.println(n*2);
+	    }
+	}
+}
