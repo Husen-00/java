36a7762269784140440860bf7465de7a036e0fc2
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-23 02:30:48 +0530

    Add good_investment_or_not.java for investment check

---
+++ CodeChef_problems/good_investement_or_not.java
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INVESTMENT
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x = input.nextInt();
+	        int y = input.nextInt();
+	        if(x >= 2*y) {
+	            System.out.println("YES");
+	        }
+	        else {
+	            System.out.println("NO");
+	        }
+	    }
+	}
+}
