08508410cd04f5c3c47661c568d8a195b1d2bbac
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-28 19:57:00 +0530

    Create Car_trip.java

---
+++ CodeChef_problems/Car_trip.java
@@ -0,0 +1,22 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CARTRIP
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x = input.nextInt();
+	        if (x<300) {
+	            System.out.println(3000);
+	        }
+	        else {
+	        System.out.println(x*10);
+	        }
+	    }
+	}
+}
