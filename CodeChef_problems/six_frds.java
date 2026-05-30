580f0eee1080e51cef15d4fc208bf4e44af24334
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-05-30 22:02:28 +0530

    Add solution for CodeChef problem SIXFRIENDS

---
+++ CodeChef_problems/six_frds.java
@@ -0,0 +1,23 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SIXFRIENDS
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int x, y;
+	        x = input.nextInt();
+	        y = input.nextInt();
+	        if(3*x <= 2*y) {
+	            System.out.println(3*x);
+	        }
+	        else {
+	            System.out.println(2*y);
+	        }
+	    }
+	}
+}
