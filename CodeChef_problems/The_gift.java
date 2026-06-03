ffc218e77ccd51c2998764e263212ba3e2b88adf
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-03 21:45:04 +0530

    Add Java solution for the GIFT problem
    Implement solution for CodeChef GIFT problem.

---
+++ CodeChef_problems/The_gift.java
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int x = input.nextInt();
+	    int n = input.nextInt();
+	    int m = input.nextInt();
+	    if(x+m>=n) {
+	        System.out.println("YES");
+	    }
+	    else {
+	        System.out.println("NO");
+	    }
+	}
+}
