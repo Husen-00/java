c3da58e6115e6d89647eb1c357a657465717e608
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-06 15:13:11 +0530

    Add The Cheaper Cab problem solution in Java

---
+++ CodeChef_problems/The_cheaper_cab.java
@@ -0,0 +1,26 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t =input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int x= input.nextInt();
+	        int y = input.nextInt();
+	        if(x<y) {
+	            System.out.println("FIRST");
+	        }
+	        
+	        else if (y<x) {
+	            System.out.println("SECOND");
+	        }
+	        else {
+	            System.out.println("ANY");
+	        }
+	    }
+	}
+}
