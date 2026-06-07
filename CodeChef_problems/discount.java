8dcc4e644a79330d6b0367035301dacc0e34606d
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-07 22:26:21 +0530

    Add discount calculation program in discount.java

---
+++ CodeChef_problems/discount.java
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int t =input.nextInt();
+	    for (int i=0; i<t; i++)  {
+	        int x = input.nextInt();
+	        System.out.println(100-x);
+	    }
+	}
+}
