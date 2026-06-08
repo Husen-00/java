971ac4b6692355f38372e477cbb9178da4c0a4b5
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-08 21:48:45 +0530

    Add heightOf_rationals.java solution

---
+++ CodeChef_problems/heightOf_rationals.java
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int a = input.nextInt();
+	    int b = input.nextInt();
+	    if (a>b) {
+	        System.out.println(a);
+	    }
+	    else {
+	        System.out.println(b);
+	    }
+	}
+}
