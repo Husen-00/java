09b8441403365ad1e39f6eb6525c64c664db1295
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-15 14:45:24 +0530

    Add find_remainder.java for calculating remainders

---
+++ CodeChef_problems/find_remainder.java
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t= input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int a = input.nextInt();
+	        int b= input.nextInt();
+	        System.out.println(a%b);
+	    }
+	}
+}
