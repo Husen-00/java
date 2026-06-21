d9ffd425479df470650aaa6c4f10204e4ed4627a
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-21 12:29:07 +0530

    Add Broken_phone.java solution for CodeChef problem

---
+++ CodeChef_problems/Broken_phone.java
@@ -0,0 +1,26 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BROKENPHONE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int x = input.nextInt();
+	        int  y= input.nextInt();
+	        if(x< y) {
+	            System.out.println("REPAIR");
+	        }
+	        else if (y<x) {
+	            System.out.println("NEW PHONE");
+	        }
+	        else {
+	            System.out.println("ANY");
+	        }
+	    }
+	}
+}
