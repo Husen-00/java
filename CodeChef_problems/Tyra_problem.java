c239f4e7d7278bfdc336fbba70c4663fc32ba25f
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-22 01:37:33 +0530

    Add Tyra_problem.java for CodeChef problem solution

---
+++ CodeChef_problems/Tyra_problem.java
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TYRE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int n= input.nextInt();
+	        int m = input.nextInt();
+	        System.out.println(2*n + 4*m);
+	    }
+	}
+}
