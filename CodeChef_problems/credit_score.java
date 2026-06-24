67f21ab6be52cdccf744a8759573b69126d49837
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-25 01:12:13 +0530

    Implement credit score evaluation in credit_score.java
    Added credit score checking logic based on input value.

---
+++ CodeChef_problems/credit_score.java
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CREDSCORE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int x = input.nextInt();
+	    if(x>=750) {
+	        System.out.println("YES");
+	    }
+	    else {
+	        System.out.println("NO");
+	    }
+	}
+}
