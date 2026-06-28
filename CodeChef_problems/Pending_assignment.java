db2763875efe67f2c8accf75e87c9a8b330bcb37
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-28 19:21:24 +0530

    Add Pending_assignment.java for CodeChef problem
    Implement solution for CodeChef assignment problem.

---
+++ CodeChef_problems/Pending_assignment.java
@@ -0,0 +1,24 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ASSIGNMNT
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for(int i=0; i<t; i++) {
+	        int x = input.nextInt();
+	        int y = input.nextInt();
+	        int z = input.nextInt();
+	        if(x*y<=z*24*60) {
+	            System.out.println("YES");
+	        }
+	        else {
+	            System.out.println("NO");
+	        }
+	    }
+	}
+}
