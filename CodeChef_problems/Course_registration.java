2f92880cbd07105e1a9cf4cb286440aff816ac77
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-06-29 22:20:57 +0530

    Add Course_registration.java for CodeChef problem

---
+++ CodeChef_problems/Course_registration.java
@@ -0,0 +1,24 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COURSEREG
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner (System.in);
+	    int t = input.nextInt();
+	    for (int i=0; i<t; i++) {
+	        int n = input.nextInt();
+	        int m = input.nextInt();
+	        int k = input.nextInt();
+	        if(n+k<=m) {
+	            System.out.println("YES");
+	        }
+	        else {
+	            System.out.println("NO");
+	        }
+	    }
+	}
+}
