8373e95712ef3bfcbfb6a8b55136d57f78bdef40
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-07-01 21:48:15 +0530

    Add increaseIQ.java for CodeChef problem solution

---
+++ CodeChef_problems/increaseIQ.java
@@ -0,0 +1,18 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INCRIQ
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner input = new Scanner(System.in);
+	    int x = input.nextInt();
+	    if(x+7 > 170) {
+	        System.out.println("YES");
+	    }
+	    else {
+	        System.out.println("NO");
+	    }
+	}
+}
